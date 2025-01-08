0d83dff16e4411eb96a36792948a4e9c869ffde6
Eloagbawe <eloagbawe@gmail.com>
2025-01-07 20:11:17 -0500

    update get and update todo

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,24 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo;
+  const { text, done } = req.body;
+
+  if (text) {
+    todo.text = text;
+  }
+
+  if (done !== undefined) {
+    todo.done = done;
+  }
+  await todo.save();
+  res.sendStatus(200);
+
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
