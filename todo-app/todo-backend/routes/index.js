6e19ece92e2a8f27d483c46d18d1adb05e7452c2
Eloagbawe <eloagbawe@gmail.com>
2025-01-08 18:51:01 -0500

    add /statistics route and implement added_todos counter

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const configs = require('../util/config')
+const redis = require('../redis')
 
 let visits = 0
 
@@ -14,5 +15,16 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+/* GET todos statistics */
+router.get('/statistics', async(req, res) => {
+  const count = await redis.getAsync('added_todos');
+
+  if (count) {
+    res.json({'added_todos': Number(count)});
+  } else {
+    res.json({'added_todos': 0});
+  }
+})
 
 module.exports = router;
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Todo } = require('../mongo')
+const { getAsync, setAsync } = require('../redis');
 const router = express.Router();
 
 /* GET todos listing. */
@@ -14,6 +15,14 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
+  const count = await getAsync('added_todos');
+
+  if (count) {
+    await setAsync('added_todos', Number(count) + 1);
+  } else {
+    await setAsync('added_todos', 1);
+  }
+
   res.send(todo);
 });
 
